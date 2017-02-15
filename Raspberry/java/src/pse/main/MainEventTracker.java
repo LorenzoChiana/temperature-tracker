5d50dea51325e29b8f5611d418d3877962aeb2d1
Matteo Minardi <matteo.minardi@studio.unibo.it>
2017-02-15 12:38:06 +0100

    Pin changed

--- Raspberry/java/src/pse/main/MainEventTracker.java
+++ Raspberry/java/src/pse/main/MainEventTracker.java
@@ -20,14 +20,14 @@ public class MainEventTracker {
 		//Light greenLed1 = new pse.modulo_lab_4_2.devices.emu.Led(4);
 		//Light greenLed2 = new pse.modulo_lab_4_2.devices.emu.Led(5);
 		//Light alarmLed = new pse.modulo_lab_4_2.devices.emu.Led(8,"red");
-		Light greenLed1 = new pse.devices.pi4j.Led(4);
-		Light greenLed2 = new pse.devices.pi4j.Led(5);
-		Light alarmLed = new pse.devices.pi4j.Led(8);
+		Light greenLed1 = new pse.devices.pi4j.Led(0);
+		Light greenLed2 = new pse.devices.pi4j.Led(2);
+		Light alarmLed = new pse.devices.pi4j.Led(3);
 		//Serial serialConnection = new pse.modulo_lab_4_2.devices.emu.SerialImpl(6, 7);		
 		SerialCommChannel serialConnection = new SerialCommChannel(args[0],9600);
 		
 		//ObservableButton button = new pse.modulo_lab_4_2.devices.emu.ObservableButton(17);
-		ObservableButton button = new pse.devices.pi4j.Button(17);
+		ObservableButton button = new pse.devices.pi4j.Button(7);
 		
 		EventTracker tracker = new EventTracker(greenLed1, greenLed2, alarmLed, button);
 		SerialReciver rec = new SerialReciver(tracker, serialConnection);
